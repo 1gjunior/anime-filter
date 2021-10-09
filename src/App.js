bbecdc8699ff4b11b43d3090508a1b29f701fd61
Gilberto Junior <junior.siilva182@gmail.com>
2021-10-09 15:03:38 -0300

    feat: :sparkles: show response api info

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
 
   useEffect(() => {
     if (text) {
-      fetch(`${api}anime?filter[text]=${text}`).then((response) =>
-        response.json().then((data) => console.log(data))
+      fetch(`${api}anime?filter[text]=${text}&page[limit]=12`).then(
+        (response) => response.json().then((response) => setInfo(response))
       );
     }
   }, [text]);
@@ -19,7 +19,19 @@ function App() {
     <div className="App">
       <h1>Animes</h1>
       <SearchInput value={text} onChange={(search) => setText(search)} />
-      {info.data && <ul></ul>}
+      {info.data && (
+        <ul className="animes-list">
+          {info.data.map((anime) => (
+            <li key={anime.id}>
+              <img
+                src={anime.attributes.posterImage.small}
+                alt={anime.attributes.canonicalTitle}
+              />
+              {anime.attributes.canonicalTitle}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
